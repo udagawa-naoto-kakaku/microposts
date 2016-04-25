7276938d018357109663a4063bc95d0c80ddc146
naoto <udagawa.naoto.kakaku@gmail.com>
2016-04-25 08:58:36 +0000

    profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -17,9 +18,24 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'プロフィールを編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmaiton)
+    params.require(:user).permit(:name, :email, :password, :password_confirmaiton, :favorite)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
